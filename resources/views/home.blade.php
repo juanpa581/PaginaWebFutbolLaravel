30331ff4acfb3675e0b1dd7df604459584298edd
juanpa581 <pablo.aguila.cedillo14@gmail.com>
2021-05-12 16:13:08 -0500

    Iniciando repositorio pagina web futbol laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,276 @@
+@extends('app')
+
+@section('content')
+<!-- MAIN -->
+<main id="main">
+        <div id="carousel" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <!-- <img src="imagenes/escuela" class="d-block w-100" alt="..."> -->
+                    <img src="imagenes/escuela.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="imagenes/escuela2.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="imagenes/escuela3.jpg" class="d-block w-100" alt="...">
+                </div>
+                <!-- <p class="d-none d-md-block">Combinamos deporte, educación y tecnología en un contexto
+                    idoneo para optimizar el rendimiento de los jugadores.
+                </p> -->
+                
+
+                <div class="overlay">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-md-6 offset-md-6 text-md-right text-center">
+                                <!-- <h1>Proffesional Soccer</h1> -->
+                                <p class="d-none d-md-block">Combinamos deporte, educación y tecnología en un contexto
+                                    idoneo para optimizar el rendimiento de los jugadores.
+                                </p>
+                                <!-- <a href="#" class="btn btn-outline-light">Quiero ser alumno</a>
+                                <button class="btn btn-tecnoeduca">Mas información</button> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+                <!-- <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a> -->
+        </div>
+        <div class="tituloparrafo">
+            <h2>¿Qué son las academias de fútbol de alto rendimiento? </h2>
+        </div>
+        <div class="parrafo">
+            <p class="d-none d-md-block">
+                Son escuelas de fútbol, centros de alto rendimiento en las que jóvenes jugadores de 13 a 22 años, se integran en un programa de fútbol que les permite tener acceso a todos los recursos necesarios para lograr ser jugador profesional. 
+                Es una academia que les ofrece educación, entrenamiento de alto rendimiento colectivo y tecnificación individual con entrenamientos adaptados a su nivel, preparación física, seguimiento nutricional y médico, competición, valoración de la evolución con asesoramiento, 
+                pruebas de fútbol y muchas más oportunidades de poder acceder a un equipo profesional.
+                    </p>
+        </div>
+        
+    </main>
+    <!-- END MAIN -->
+
+
+    <!-- CURSOS -->
+    <section id="academia" class="mb-4">
+        <div class="container">
+            <div class="row">
+            </div> <br> <br>
+<!-- segunda captura de imagenes en movimiento -->
+
+      <div class="col text-center text-uppercase">
+           <h2>Nuestra Academia</h2>
+      </div>
+            <main id="main">
+                <div id="carousel" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="imagenes/Instalaciones.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imagenes/instalaciones2.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imagenes/instalaciones3.jpg" class="d-block w-100" alt="...">
+                        </div>
+        
+                        <!-- <div class="overlay">
+                            <div class="container">
+                                <div class="row align-items-center">
+                                    <div class="col-md-6 offset-md-6 text-md-right text-center"> -->
+                                        <!-- <h1>Proffesional Soccer</h1> -->
+                                        <!-- <p class="d-none d-md-block">Combinamos deporte, educación y tecnología en un contexto
+                                            idoneo para optimizar el rendimiento de los jugadores.
+                                        </p>
+                                        <a href="#" class="btn btn-outline-light">Quiero ser alumno</a>
+                                        <button class="btn btn-tecnoeduca">Mas información</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> -->
+        
+                    </div>
+                    <div class="parrafodos">
+                        <p class="d-none d-md-block">Es una de las mejores formas que tienes si buscas el éxito como jugador de fútbol. 
+                            La posibilidad de tener un programa personalizado de fútbol diseñado de acuerdo a tus necesidades te permitirá mejorar tus cualidades como deportista de élite, 
+                            de obtener oportunidades de probar directamente con los mejores clubes de fútbol de España o Europa. El estar preparado y evaluado te permite afrontar con más posibilidades las pruebas además de poder directamente acceder a ellas. 
+                            Experimentar la metodología "fútbol españa". Te permite compaginar estudios y entrenamientos de alto rendimiento. Te permite acceder a becas deportivas en USA o Inglaterra para continuar tus entrenamientos mientras estudias una carrera universitaria o un grado, 
+                            adaptándose a fútbol inglaterra o de estados unidos.
+                        </p>
+                    </div>
+
+            <!-- <div class="row">
+                <div class="col-12 col-md-6 col-lg-4 mb-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/html.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">HTML</h5>
+                          <p class="card-text">
+                              HTML es un lenguaje de marcado que se utiliza para el desarrollo de paginas de internet.
+                          </p>
+                          <a href="#" class="btn btn-tecnoeduca" data-toggle="modal" data-target="#HTMLModal">Ver curso</a>
+                        </div>
+                      </div>
+                </div> -->
+
+
+                <!-- <div class="col-12 col-md-6 col-lg-4 mb-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/css.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">CSS</h5>
+                          <p class="card-text">
+                              Hojas de estilo en cascada, es el lenguaje de estilos utilizado para describir la presentación de documentos HTML.
+                          </p>
+                          <a href="#" class="btn btn-tecnoeduca">Ver curso</a>
+                        </div>
+                      </div>
+                </div> -->
+
+
+                <!-- <div class="col-12 col-md-6 col-lg-4 mb-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="images/javascript.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">JavaScript</h5>
+                          <p class="card-text">
+                              Es un lenguaje de programación ligero, interpretado, o compilado con funciones de primera clase.
+                          </p>
+                          <a href="#" class="btn btn-tecnoeduca">Ver curso</a>
+                        </div>
+                      </div>
+                </div> -->
+            </div>
+        </div>
+    </section>
+    <!-- END CURSOS -->
+
+    <!-- inicio tercer imagenes en movimiento -->
+    <section id="entrenamientos" class="mb-4">
+        <div class="container">
+            <div class="row">
+                <!-- <div class="col text-center text-uppercase">
+                    <small>Conoce nuestras instalaciones, visita nuestra</small><h2>Academia</h2>
+                </div> -->
+            </div> <br> <br>
+<!-- segunda captura de imagenes en movimiento -->
+
+      <div class="col text-center text-uppercase">
+           <h2>¿Como son nuestros entrenamientos?</h2>
+      </div>
+            <main class="entrenamientos" id="tercervista">
+                <div id="carousel" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="imagenes/entrenamientos.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imagenes/entrenamientos2.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imagenes/entrenamientos3.jpg" class="d-block w-100" alt="...">
+                        </div>
+
+        
+                    </div>
+                    <div class="parrafodos">
+                        <p class="d-none d-md-block">La mayor preocupación de un punto de vista metodológico es garantizar que los jugadores de nuestro programa de alto rendimiento están involucrados en las tareas y logran un determinado número de horas de entrenamiento al largo de una temporada.
+                            Estudios han revelado que para lograr la excelencia hay que estar expuesto a un determinado entorno durante un largo periodo de tiempo. 
+                            Entre estos estudios se pueden encontrar la teoría de las 10 000 horas de Ericson. 
+                            Así, es muy importante hacer una gestión adecuada del binomio desempeño-recuperación, garantizando que los jugadores están en condiciones para enfrentar los entrenamientos y la competición.
+                            Todo el proceso es dirigido por nuestra filosofía de juego, el elemento causal del proceso, lo que nos permite dar un sentido a la formación de los jugadores y potenciar sus características individuales. 
+                            Cada principio táctico presupone una exigencia técnica, fisiológica y mental. La recreación de estos principios tácticos permite que los jugadores mejoren muchos todos los aspectos de su juego y se adapten con mucha más facilidad a las exigencias del futbol profesional. 
+                            Al largo de una semana es expectable que los jugadores entrenen entre 10-15 horas, en función de los resultados de los testes que se hacen periódicamente para evaluar el estado físico y mental de los jugadores, y que se trabajen aspectos individuales, sectoriales, intersectoriales y colectivos.
+                        </p>
+                    </div>
+            </div>
+        </div>
+    </section>
+    <!-- fin imagenes en movimiento -->
+
+    <!-- clase online -->
+    <section id="perfil">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 pl=0 pr=0">
+                    <img src="imagenes/escuela4.jpg" alt="">
+
+                </div>
+                <div class="col-lg-6 col-12 pb-4">
+                    <h2>¿Cuál es el perfil del candidato ideal para una escuela de fútbol de alto rendimiento?</h2>
+                    <p>
+                        Es un joven con excelentes actitudes deportivas que tiene como objetivo ser jugador profesional de fútbol y que desea seguir su formación académica para asegurarse un futuro profesional. También acceden jugadores que quieren mejorar para poder acceder a becas universitarias . 
+                        El objetivo final debe ser convertirse en un futbolista profesional, con estudios universitarios y estudios reglados, en temas deportivos como preparación física, tecnificación fútbol, dirección de clubs profesionales, curso de entrenador.
+                    </p>
+                    <!-- <a href="" class="btn btn-outline-light"> Registrarme</a> -->
+
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <br> <br>
+    <!-- fin clase online -->
+
+
+    <!-- inicio cuadros de registro -->
+    <section id="registros" class="mb-4">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    <h2>Conocenos</h2> <br>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-4 mb-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="imagenes/torneo.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Torneos</h5>
+                          <p class="card-text">
+                              Participa en los torneos mas reconocidos a nivel regional y estatal qu esta escuela te puede ofrecer.
+                          </p>
+                          <a href="#" class="btn btn-visoria" data-toggle="modal" data-target="#TorneoModal">Registrarme</a>
+                        </div>
+                      </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4 mb-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="imagenes/torneo2.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Equipos</h5>
+                          <p class="card-text">
+                              Registra a tu equipo para comenzar con los mejores entrenamientos. <br> <br>
+                          </p>
+                          <a href="#" class="btn btn-visoria" data-toggle="modal" data-target="#EquiposModal">Registrar</a>
+                        </div>
+                      </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4 mb-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="imagenes/torneo3.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <h5 class="card-title">Nosotros</h5>
+                          <p class="card-text">
+                              Conocenos mejor y quitate esas dudas <br> <br> <br>
+                          </p>
+                          <a href="#" class="btn btn-visoria" data-toggle="modal" data-target="#NosotrosModal">Ver</a>
+                        </div>
+                      </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- fin cuadro -->
+@endsection
---
+++ resources/views/modales.blade.php
@@ -0,0 +1,206 @@
+ <!-- inicio del modal -->
+    <!-- modal uno -->
+    <div class="modal fade" id="TorneoModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Disponible proximamente</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <!-- <div class="modal-body">
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      Introduccion a html
+                      <span class="badge badge-primary badge-pill">2h</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      formatos de pagina
+                      <span class="badge badge-primary badge-pill">2h</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      formatos de texto
+                      <span class="badge badge-primary badge-pill">1h</span>
+                    </li>
+
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        imagenes
+                        <span class="badge badge-primary badge-pill">3h</span>
+                      </li>
+                      <li class="list-group-item d-flex justify-content-between align-items-center">
+                        tablas y formularios
+                        <span class="badge badge-primary badge-pill">3h</span>
+                      </li>
+                      <li class="list-group-item d-flex justify-content-between align-items-center">
+                        grillas y frameworks
+                        <span class="badge badge-primary badge-pill">5h</span>
+                      </li>
+
+                    </ul>
+                    <div class="alert alert-success pb-2" role="alert">
+                        17 horas de curso
+                      </div>
+            </div> -->
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- modal cierre -->
+    <!-- fin del modal uno -->
+
+    <!-- modal dos -->
+    <div class="modal fade" id="EquiposModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Disponible proximamente</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <!-- <div class="modal-body">
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      Introduccion a html
+                      <span class="badge badge-primary badge-pill">2h</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      formatos de pagina
+                      <span class="badge badge-primary badge-pill">2h</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      formatos de texto
+                      <span class="badge badge-primary badge-pill">1h</span>
+                    </li>
+
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        imagenes
+                        <span class="badge badge-primary badge-pill">3h</span>
+                      </li>
+                      <li class="list-group-item d-flex justify-content-between align-items-center">
+                        tablas y formularios
+                        <span class="badge badge-primary badge-pill">3h</span>
+                      </li>
+                      <li class="list-group-item d-flex justify-content-between align-items-center">
+                        grillas y frameworks
+                        <span class="badge badge-primary badge-pill">5h</span>
+                      </li>
+
+                    </ul>
+                    <div class="alert alert-success pb-2" role="alert">
+                        17 horas de curso
+                      </div>
+            </div> -->
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- fin modal dos -->
+
+    <!-- Inicio Modal 3 -->
+    <div class="modal fade" id="NosotrosModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Disponible proximamente</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <!-- <div class="modal-body">
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      Introduccion a html
+                      <span class="badge badge-primary badge-pill">2h</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      formatos de pagina
+                      <span class="badge badge-primary badge-pill">2h</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      formatos de texto
+                      <span class="badge badge-primary badge-pill">1h</span>
+                    </li>
+
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        imagenes
+                        <span class="badge badge-primary badge-pill">3h</span>
+                      </li>
+                      <li class="list-group-item d-flex justify-content-between align-items-center">
+                        tablas y formularios
+                        <span class="badge badge-primary badge-pill">3h</span>
+                      </li>
+                      <li class="list-group-item d-flex justify-content-between align-items-center">
+                        grillas y frameworks
+                        <span class="badge badge-primary badge-pill">5h</span>
+                      </li>
+
+                    </ul>
+                    <div class="alert alert-success pb-2" role="alert">
+                        17 horas de curso
+                      </div>
+            </div> -->
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- fin modal 3 -->
+
+    <!-- modal -->
+    <div class="modal fade" id="HTMLModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+                
+              <!-- <h5 class="modal-title" id="exampleModalLabel">Temario</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      Introduccion a html
+                      <span class="badge badge-primary badge-pill">2h</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      formatos de pagina
+                      <span class="badge badge-primary badge-pill">2h</span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                      formatos de texto
+                      <span class="badge badge-primary badge-pill">1h</span>
+                    </li>
+
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        imagenes
+                        <span class="badge badge-primary badge-pill">3h</span>
+                      </li>
+                      <li class="list-group-item d-flex justify-content-between align-items-center">
+                        tablas y formularios
+                        <span class="badge badge-primary badge-pill">3h</span>
+                      </li>
+                      <li class="list-group-item d-flex justify-content-between align-items-center">
+                        grillas y frameworks
+                        <span class="badge badge-primary badge-pill">5h</span>
+                      </li>
+
+                    </ul> 
+                     <div class="alert alert-success pb-2" role="alert">
+                        17 horas de curso
+                      </div>
+             </div>-->
+             <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- modal cierre -->
